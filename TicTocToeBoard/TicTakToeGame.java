e13d63a48bffc4a54d6625d81caffb793f6bd3aa
prashant169 <prashantphad2018@gmail.com>
2021-10-24 19:04:49 -0800

    Added:Player choice move in the board

--- TicTocToeBoard/TicTakToeGame.java
+++ TicTocToeBoard/TicTakToeGame.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class TicTakToeGame {
 	static char[] board = new char[10];
+	static Scanner scanner = new Scanner(System.in);
+	public static char playerChoice;
 
 	private static void ticTakToeInitialization() {
 		for (int i = 1; i < board.length; i++) {
@@ -12,13 +14,12 @@ public class TicTakToeGame {
 	}
 
 	private static void playerChose() {
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("Player choose X or O");
-		char playerChose = scanner.next().charAt(0);
-		if (playerChose == 'X' || playerChose == 'x') {
-			System.out.println("Player Chosen :" + playerChose);
+		playerChoice = scanner.next().charAt(0);
+		if (playerChoice == 'X' || playerChoice == 'x') {
+			System.out.println("Player Chosen :" + playerChoice);
 			System.out.println("Computer Choice O");
-		} else if (playerChose == 'O' || playerChose == 'o') {
+		} else if (playerChoice == 'O' || playerChoice == 'o') {
 			System.out.println("Player Chosen O");
 			System.out.println("computer Choice X ");
 		} else
@@ -33,10 +34,24 @@ public class TicTakToeGame {
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 	}
 
+	private static void playerMove() {
+		System.out.println("Select Position in Index to Move in Board (1 to 9) : ");
+		int indexNumber = scanner.nextInt();
+		if (indexNumber < 1 || indexNumber > 9) {
+			System.out.println("You Entered Invalid Position ");
+			playerMove();
+		} else {
+			board[indexNumber] = playerChoice;
+			showBoard();
+		}
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tak Toe Game");
 		ticTakToeInitialization();
 		playerChose();
 		showBoard();
+		playerMove();
 	}
 }
